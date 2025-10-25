b8b14aa6584c236883e69b3547ae1b9bcebd6d65
bustercheeks7 <148999808+bustercheeks7@users.noreply.github.com>
2025-10-26 01:03:53 +0300

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  output: "standalone", // disables static export for SSR/dynamic pages
+};
 
-export default nextConfig
+module.exports = nextConfig;
